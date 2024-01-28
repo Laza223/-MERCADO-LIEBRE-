4f5ec0c9b16ea09b272b2be6b556397a0f048e3b
Laza223 <lazarofeijoo2004@gmail.com>
2024-01-28 15:26:59 -0300

    Subiendo ml3

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const app = express()
 
 app.use(express.static("public"))
 
-app.listen(port, () => console.log("https://localhost:3030"))
+app.listen(port, () => console.log("http://localhost:3030/"))
 
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "/views/home.html"))
